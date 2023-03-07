d42c48bcf23bf98073f1f4079e8b8eae83f52b7b
BangBang-e <youngjae7133@gmail.com>
2023-03-07 17:03:06 +0900

    FE_Fix: prettier 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,25 +12,25 @@ import MyPharmacy from "./Pages/Pharm/MyPharmacy";
 import Reports from "./Pages/Admin/Reports";
 import Users from "./Pages/Admin/Users";
 
- function App () {
-     return (
-       <>
-       <BrowserRouter>
+function App() {
+  return (
+    <>
+      <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/sign_up' element={<SignUp/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/user-general_info' element={<GeneralInfo/>}></Route>
-          <Route path='/user-my_drugs' element={<MyDrugs/>}></Route>
-          <Route path='/user-my_enquiries' element={<MyEnquiries/>}></Route>
-          <Route path='/user-my_info' element={<MyInfo/>}></Route>
-          <Route path='/pharm-enquiries' element={<Enquiries/>}></Route>
-          <Route path='/pharm-my_pharmacy' element={<MyPharmacy/>}></Route>
-          <Route path='/admin-reports' element={<Reports/>}></Route>
-          <Route path='/admin-users' element={<Users/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign_up" element={<SignUp />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/user-general_info" element={<GeneralInfo />}></Route>
+          <Route path="/user-my_drugs" element={<MyDrugs />}></Route>
+          <Route path="/user-my_enquiries" element={<MyEnquiries />}></Route>
+          <Route path="/user-my_info" element={<MyInfo />}></Route>
+          <Route path="/pharm-enquiries" element={<Enquiries />}></Route>
+          <Route path="/pharm-my_pharmacy" element={<MyPharmacy />}></Route>
+          <Route path="/admin-reports" element={<Reports />}></Route>
+          <Route path="/admin-users" element={<Users />}></Route>
         </Routes>
-       </BrowserRouter>
-       </>
-     )
-   } 
-   export default App
+      </BrowserRouter>
+    </>
+  );
+}
+export default App;
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 import { Provider } from "react-redux";
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { store } from './Redux/Store/store';
-import './main.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { store } from "./Redux/Store/store";
+import "./main.css";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store ={store}>
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>,
-)
+);
