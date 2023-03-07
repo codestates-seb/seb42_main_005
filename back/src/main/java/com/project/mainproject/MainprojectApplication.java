1b153999983fd21befa1b76b276a91d786bfbadf
JiHyeong23 <ekdl1120@gmail.com>
2023-03-07 17:14:26 +0900

    BE_Feat: Auditing 적용

--- back/src/main/java/com/project/mainproject/MainprojectApplication.java
+++ back/src/main/java/com/project/mainproject/MainprojectApplication.java
@@ -2,7 +2,12 @@ package com.project.mainproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +17,9 @@ public class MainprojectApplication {
 		SpringApplication.run(MainprojectApplication.class, args);
 	}
 
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.ofNullable(UUID.randomUUID().toString());
+	}
 }
