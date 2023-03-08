e5800d388859f70feca7e0e9a0e19dbf51fe73d4
SigLee <lgh941107@gmail.com>
2023-03-08 16:35:17 +0900

    BE_Fix: 오타 수정 컬럼명 오타 수정

--- back/src/main/java/com/project/mainproject/store/entity/Store.java
+++ back/src/main/java/com/project/mainproject/store/entity/Store.java
@@ -31,49 +31,49 @@ public class Store {
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name="startTime",column = @Column(name = "MONDAY_OPERATING_START")),
-            @AttributeOverride(name ="endTime",column = @Column(name = "MONDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "MONDAY_OPERATING_END"))
     })
     private OperatingTime mondayOperating;
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name="startTime",column = @Column(name = "TUESDAY_OPERATING_START")),
-            @AttributeOverride(name ="endTime",column = @Column(name = "TUESDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "TUESDAY_OPERATING_END"))
     })
     private OperatingTime tuesdayOperating;
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name="startTime",column = @Column(name = "WEDNESDAY_OPERATING_START")),
-            @AttributeOverride(name ="endTime",column = @Column(name = "WEDNESDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "WEDNESDAY_OPERATING_END"))
     })
     private OperatingTime wednesdayOperating;
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name="startTime",column = @Column(name = "THURSDAY_OPERATING_START")),
-            @AttributeOverride(name ="endTime",column = @Column(name = "THURSDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "THURSDAY_OPERATING_END"))
     })
     private OperatingTime thursdayOperating;
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name="startTime",column = @Column(name = "FRIDAY_OPERATING_START")),
-            @AttributeOverride(name ="endTime",column = @Column(name = "FRIDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "FRIDAY_OPERATING_END"))
     })
     private OperatingTime fridayOperating;
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name="startTime",column = @Column(name = "SATURDAY_OPERATING_START")),
-            @AttributeOverride(name ="endTime",column = @Column(name = "SATURDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "SATURDAY_OPERATING_END"))
     })
     private OperatingTime saturdayOperating;
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name = "startTime", column = @Column(name = "SUNDAY_OPERATING_START")),
-            @AttributeOverride(name = "endTime", column = @Column(name = "SUNDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "SUNDAY_OPERATING_END"))
     })
     private OperatingTime sundayOperating;
     @Enumerated
     @AttributeOverrides({
             @AttributeOverride(name="startTime",column = @Column(name = "HOLIDAY_OPERATING_START")),
-            @AttributeOverride(name ="endTime",column = @Column(name = "HOLIDAY_OPERATING_END")),
+            @AttributeOverride(name = "endTime", column = @Column(name = "HOLIDAY_OPERATING_END"))
     })
     private OperatingTime holidayOperating;
 
