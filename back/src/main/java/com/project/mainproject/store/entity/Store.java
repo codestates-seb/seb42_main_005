bf669dc4e71f99b783c729f2a715044a5737f31f
SigLee <lgh941107@gmail.com>
2023-03-08 16:22:04 +0900

    BE_Feat: Store 엔티티 생성 Store PickedStore 객체 생성

---
+++ back/src/main/java/com/project/mainproject/store/entity/Store.java
@@ -0,0 +1,86 @@
+package com.project.mainproject.store.entity;
+
+import com.project.mainproject.VO.OperatingTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Store {
+    @Id
+    @GeneratedValue
+    private Long storeIdx;
+
+    private String hpid;
+
+    private String name;
+
+    private String address;
+
+    private Double longitude;
+
+    private Double latitude;
+
+    private String tel;
+
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "MONDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "MONDAY_OPERATING_END")),
+    })
+    private OperatingTime mondayOperating;
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "TUESDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "TUESDAY_OPERATING_END")),
+    })
+    private OperatingTime tuesdayOperating;
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "WEDNESDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "WEDNESDAY_OPERATING_END")),
+    })
+    private OperatingTime wednesdayOperating;
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "THURSDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "THURSDAY_OPERATING_END")),
+    })
+    private OperatingTime thursdayOperating;
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "FRIDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "FRIDAY_OPERATING_END")),
+    })
+    private OperatingTime fridayOperating;
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "SATURDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "SATURDAY_OPERATING_END")),
+    })
+    private OperatingTime saturdayOperating;
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "HOLIDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "HOLIDAY_OPERATING_END")),
+    })
+    private OperatingTime sundayOperating;
+    @Enumerated
+    @AttributeOverrides({
+            @AttributeOverride(name="startTime",column = @Column(name = "HOLIDAY_OPERATING_START")),
+            @AttributeOverride(name ="endTime",column = @Column(name = "HOLIDAY_OPERATING_END")),
+    })
+    private OperatingTime holidayOperating;
+
+
+    //추 후 변경 필요
+    private Boolean isOperatingHoliday;
+
+    private String etc;
+
+}
--- back/src/main/java/com/project/mainproject/user/entity/PickedStore.java
+++ back/src/main/java/com/project/mainproject/user/entity/PickedStore.java
@@ -1,5 +1,6 @@
 package com.project.mainproject.user.entity;
 
+import com.project.mainproject.store.entity.Store;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,7 +22,10 @@ public class PickedStore {
 
     private String storeId;
 
+    //연관관계 매핑
     @ManyToOne(fetch = LAZY)
     private Normal normal;
 
+    @ManyToOne(fetch =LAZY)
+    private Store store;
 }
