171345689ee6de04512d9ef2dec141cbd5afbf0c
yesolll <9595kys@gmail.com>
2023-03-09 16:02:12 +0900

    BE_Feat: Result Status 정상처리 추가

--- back/src/main/java/com/project/mainproject/enums/ResultStatus.java
+++ back/src/main/java/com/project/mainproject/enums/ResultStatus.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor
 public enum ResultStatus {
+
+    PROCESS_COMPLETED(OK, "정상처리 되었습니다."),
+    CREATE_COMPLETED(CREATED, "정상처리 되었습니다."),
+    DELETE_COMPLETED(NO_CONTENT, "정상처리 되었습니다."),
+
     MEMBER_NOT_FOUND(NOT_FOUND, "Member Not Found"),
     MEMBER_EXIST(CONFLICT, "Member is already Exist!"),
     MEMBER_JWT_EXIST(CONFLICT, "JWT Registry Member is Exist")
