3a7aa747a2a1126a8f08c3f3c9312c08e1ef4117
tjddmssl <tjddmssl910103@gmail.com>
2023-03-09 23:47:47 +0900

    FE_feat/Pharmlists 홈에서 Pharmlists불러온거 지움

--- front/src/Pages/Home.tsx
+++ front/src/Pages/Home.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import PharmLists from "../Components/PharmList/PharmLists";
 
 export default function Home() {
-  return (
-    <div>
-      <PharmLists />
-    </div>
-  );
+  return <div></div>;
 }
