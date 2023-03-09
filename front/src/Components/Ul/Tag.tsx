955505c8c2e7696cee57a2fcbf6899c5df1d4484
tjddmssl <tjddmssl910103@gmail.com>
2023-03-09 23:39:58 +0900

    FE_feat/Pharmlists PharmLists컴포넌트 화면구현함

--- front/src/Components/Ul/Tag.tsx
+++ front/src/Components/Ul/Tag.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+//태그들 => 영재님이 함!! 쓰지마!!!
+import React from "react";
+import styled from "styled-components";
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <Tags>
+      <Tag>친절해요</Tag>
+      <Tag>주차가능</Tag>
+      <Tag>꼼꼼해요</Tag>
+    </Tags>
   );
 }
 
+const Tags = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+`;
+const Tag = styled.li`
+  list-style: none;
+  border: 4px solid var(--blue-400);
+  border-radius: 30px;
+  margin-right: 20px;
+  padding: 4px;
+  font-size: 15px;
+  color: var(--blue-400);
+  :hover {
+    background-color: var(--blue-100);
+  }
+`;
--- front/src/Pages/Home.tsx
+++ front/src/Pages/Home.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import PharmLists from "../Components/PharmList/PharmLists";
 
 export default function Home() {
   return (
     <div>
+      <PharmLists />
     </div>
   );
 }
-
