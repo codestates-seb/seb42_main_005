1d93fba2219b87a0963e7f87e163c0ae607e46b0
yesolll <9595kys@gmail.com>
2023-03-09 16:03:03 +0900

    BE_Feat: Uri Creator Util 추가

---
+++ back/src/main/java/com/project/mainproject/utils/UriCreator.java
@@ -0,0 +1,15 @@
+package com.project.mainproject.utils;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class UriCreator {
+    public static URI createUri(String defaultUrl, long resourceId) {
+        return UriComponentsBuilder
+                .newInstance()
+                .path(defaultUrl + "/{resource-id}")
+                .buildAndExpand(resourceId)
+                .toUri();
+    }
+}
