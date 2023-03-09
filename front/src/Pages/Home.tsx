63ec6aa620cc6f2ec91c43e5f266baf6c2e9b1fe
tjddmssl <tjddmssl910103@gmail.com>
2023-03-10 01:41:59 +0900

    FE_feat/Pharmlists

--- front/src/Pages/Home.tsx
+++ front/src/Pages/Home.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import PharmLists from "../Components/PharmList/PharmLists";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <PharmLists />
+    </div>
+  );
 }
