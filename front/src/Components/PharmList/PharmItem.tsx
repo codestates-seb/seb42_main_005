1b3fe1fe8d4ca06b38cc29774fa1cbbca00f4b27
BangBang-e <youngjae7133@gmail.com>
2023-03-12 02:35:05 +0900

    FE_style/pharmlist 약국리스트 레이아웃 및 css 수정

--- front/src/Components/PharmList/PharmItem.tsx
+++ front/src/Components/PharmList/PharmItem.tsx
@@ -7,22 +7,18 @@ import PharmDetail from "../Modal/PharmDetail";
 export default function PharmItem() {
   const [isModalUp, setIsModalUp] = useState(false);
   const [like, setLike] = useState(false);
-  const heart: boolean = true;
+
   return (
     <PharmCard>
       {isModalUp ? (
         <PharmDetail isModalUp={isModalUp} setIsModalUp={setIsModalUp} like={like} setLike={setLike} />
       ) : null}
-      <HeartPosition>
-        <HeartButton>
-          {heart ? (
-            <img className="heart_img" alt="heart" src="Images/Heart.png" />
-          ) : (
-            <img className="unheart_img" alt="unheart" src="Images/UnHeart.png" />
-          )}
-        </HeartButton>
-      </HeartPosition>
-      <img className="pharm_img" src="./Images/random.png" alt="고심약국" onClick={() => setIsModalUp(!isModalUp)} />
+      <InfoImgContainer>
+        <Img src="./Images/random.png" alt="고심약국" onClick={() => setIsModalUp(!isModalUp)} />
+        <LikeButton onClick={() => setLike(!like)}>
+          {like ? <img src="./Images/Heart.png" /> : <img src="./Images/UnHeart.png" />}
+        </LikeButton>
+      </InfoImgContainer>
       <PharmTitleBox>
         <PharmName onClick={() => setIsModalUp(!isModalUp)}>킹갓 약국</PharmName>
         <PharmRank />
@@ -43,6 +39,7 @@ const PharmCard = styled.div`
   height: 25rem;
   display: flex;
   flex-direction: column;
+  margin-top: 35px;
   border-bottom: 1px solid var(--black-100);
   .pharm_img {
     background-color: var(--black-200);
@@ -54,18 +51,30 @@ const PharmCard = styled.div`
     border-radius: 10px;
   }
 `;
-//하트 버튼!!
-const HeartPosition = styled.div`
-  position: relative;
-`;
-const HeartButton = styled.button`
-  all: unset;
-  cursor: pointer;
 
-  img {
-    position: absolute;
-    right: 1rem;
-    top: 2.5rem;
+const InfoImgContainer = styled.div`
+  cursor: pointer;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 5px;
+  border-bottom: 1px solid var(--black-100);
+`;
+const Img = styled.img`
+  object-fit: cover;
+  width: 23.75rem;
+  height: 15.625rem;
+  border-radius: 5px;
+`;
+const LikeButton = styled.span`
+  position: absolute;
+  right: 36px;
+  top: 12px;
+  width: 20px;
+  @media (max-width: 768px) {
+    right: 60px;
+    top: 11px;
   }
 `;
 
@@ -73,10 +82,12 @@ const HeartButton = styled.button`
 const PharmTitleBox = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-top: 15px;
   padding: 0 15px;
 `;
 
 const PharmName = styled.div`
+  cursor: pointer;
   font-size: 1.56rem;
   font-weight: bold;
 `;
