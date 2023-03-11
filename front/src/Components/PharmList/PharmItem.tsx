823e1e318ffc08c2b7e44623a555cb4e22cc8ca0
BangBang-e <youngjae7133@gmail.com>
2023-03-11 23:54:26 +0900

    FE_style/pharmlist CSS 수정

--- front/src/Components/PharmList/PharmItem.tsx
+++ front/src/Components/PharmList/PharmItem.tsx
@@ -1,5 +1,129 @@
-import React from "react";
+import styled from "styled-components";
+import Tag from "../Ul/Tag";
+import PharmRank from "../Ul/PharmRank";
+
+import Input from "../Ul/Input";
+import Textarea from "../Ul/Textarea";
 
 export default function PharmItem() {
-  return <div></div>;
+  const heart: boolean = true;
+  return (
+    <ItemContainer>
+      <PharmCard>
+        <HeartPosition>
+          <HeartButton>
+            {heart ? (
+              <img className="heart_img" alt="heart" src="Images/Heart.png" />
+            ) : (
+              <img className="unheart_img" alt="unheart" src="Images/UnHeart.png" />
+            )}
+          </HeartButton>
+        </HeartPosition>
+        <img className="pharm_img" />
+        <PharmTitleBox>
+          <PharmName>킹갓 약국</PharmName>
+          <PharmRank />
+        </PharmTitleBox>
+        <TagContainer>
+          <Tag idx={0} />
+          <Tag idx={1} />
+          <Tag idx={2} />
+          <Tag idx={3} />
+        </TagContainer>
+      </PharmCard>
+      <PharmCard>
+        <img className="pharm_img" />
+        <PharmTitleBox>
+          <PharmName>킹갓 약국</PharmName>
+          <PharmRank />
+        </PharmTitleBox>
+        <TagContainer>
+          <Tag idx={0} />
+          <Tag idx={1} />
+          <Tag idx={2} />
+          <Tag idx={3} />
+        </TagContainer>
+      </PharmCard>
+      <PharmCard>
+        <img className="pharm_img" />
+        <PharmTitleBox>
+          <PharmName>킹갓 약국</PharmName>
+          <PharmRank />
+        </PharmTitleBox>
+        <TagContainer>
+          <Tag idx={0} />
+          <Tag idx={1} />
+          <Tag idx={2} />
+          <Tag idx={3} />
+        </TagContainer>
+      </PharmCard>
+
+      <PharmCard>
+        <img className="pharm_img" />
+        <PharmTitleBox>
+          <PharmName>킹갓 약국</PharmName>
+          <PharmRank />
+        </PharmTitleBox>
+        <TagContainer>
+          <Tag idx={0} />
+          <Tag idx={1} />
+          <Tag idx={2} />
+          <Tag idx={3} />
+        </TagContainer>
+      </PharmCard>
+    </ItemContainer>
+  );
 }
+const ItemContainer = styled.div``;
+//약국카드
+const PharmCard = styled.div`
+  width: 25rem;
+  height: 25rem;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid var(--black-100);
+  .pharm_img {
+    background-color: var(--black-200);
+    width: 23.75rem;
+    height: 15.625rem;
+    display: flex;
+    justify-content: center;
+    margin: 30px auto 20px auto;
+    border-radius: 10px;
+  }
+`;
+//하트 버튼!!
+const HeartPosition = styled.div`
+  position: relative;
+`;
+const HeartButton = styled.button`
+  all: unset;
+  cursor: pointer;
+
+  img {
+    position: absolute;
+    right: 1rem;
+    top: 2.5rem;
+  }
+`;
+
+//약국 타이틀
+const PharmTitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15px;
+`;
+
+const PharmName = styled.div`
+  font-size: 1.56rem;
+  font-weight: bold;
+`;
+
+//태그전체
+const TagContainer = styled.div`
+  margin: 15px 0 10px 0;
+  padding: 0 15px;
+  & > * {
+    margin: 0 2px;
+  }
+`;
--- front/src/Components/PharmList/PharmLists.tsx
+++ front/src/Components/PharmList/PharmLists.tsx
@@ -1,11 +1,7 @@
 //홈화면 옆에 약국 리스트
 import styled from "styled-components";
+import PharmItem from "./PharmItem";
 import { BsSearch } from "react-icons/bs";
-import Tag from "../Ul/Tag";
-import PharmRank from "../Ul/PharmRank";
-
-import Input from "../Ul/Input";
-import Textarea from "../Ul/Textarea";
 
 //데이터 들어오면 map으로 돌리기전에 하드코딩으로 두개해놨음
 export default function PharmLists() {
@@ -15,207 +11,183 @@ export default function PharmLists() {
   const heart: boolean = true;
 
   return (
-    <Container>
+    <ListContainer>
       <ContainerWrap>
-        <SearchContainer>
-          <div>
-            <BsSearch className="searchIcon" />
-          </div>
-          <SearchInput placeholder="약국 검색.." />
-        </SearchContainer>
-        <PharmHeadContainer>
-          <div>
-            <Button>
-              <img className="ourpharm_img" alt="pharm" src="Images/OurPharmGo.png" />
-              <span className="ourpharm">우리 약국</span>
-            </Button>
-          </div>
-          <FilterButtons>
-            <FilterButton className="filter_close">가까운순</FilterButton>
-            <FilterButton className="filter_popular">리뷰많은순</FilterButton>
-            <FilterButton className="filter_star">별점높은순</FilterButton>
-          </FilterButtons>
-        </PharmHeadContainer>
-        <PharmCard>
-          <HeartPosition>
-            <HeartButton>
-              {heart ? (
-                <img className="heart_img" alt="heart" src="Images/Heart.png" />
-              ) : (
-                <img className="unheart_img" alt="unheart" src="Images/UnHeart.png" />
-              )}
-            </HeartButton>
-          </HeartPosition>
-          <img className="pharm_img" />
-          <PharmTitleBox>
-            <PharmName>킹갓 약국</PharmName>
-            <PharmRank />
-          </PharmTitleBox>
-          <TagContainer>
-            <Tag idx={0} />
-            <Tag idx={1} />
-            <Tag idx={2} />
-            <Tag idx={3} />
-          </TagContainer>
-        </PharmCard>
-        <PharmCard>
-          <img className="pharm_img" />
-          <PharmTitleBox>
-            <PharmName>킹갓 약국</PharmName>
-            <PharmRank />
-          </PharmTitleBox>
-          <TagContainer>
-            <Tag idx={0} />
-            <Tag idx={1} />
-            <Tag idx={2} />
-            <Tag idx={3} />
-          </TagContainer>
-        </PharmCard>
+        <EmptyContainer>
+          <span className="partition" />
+        </EmptyContainer>
+        <PharmContainer>
+          <PharmHeadContainer>
+            <SearchContainer>
+              <div>
+                <BsSearch className="searchIcon" />
+              </div>
+              <SearchInput placeholder="약국 검색.." />
+            </SearchContainer>
+            <ButtonContainer>
+              <Button>
+                <img className="ourpharm_img" alt="pharm" src="Images/OurPharmGo.png" />
+                <span className="ourpharm">우리 약국</span>
+              </Button>
+              <FilterButtons>
+                <FilterButton>가까운순</FilterButton>
+                <span className="partition" />
+                <FilterButton>리뷰많은순</FilterButton>
+                <span className="partition" />
+                <FilterButton>별점높은순</FilterButton>
+              </FilterButtons>
+            </ButtonContainer>
+          </PharmHeadContainer>
+          <PharmItemContainer>
+            <PharmItem></PharmItem>
+          </PharmItemContainer>
+        </PharmContainer>
       </ContainerWrap>
-    </Container>
+    </ListContainer>
   );
 }
 
 //전체 컨테이너
-const Container = styled.div`
-  width: 37.5rem;
-  display: block;
-  position: relative;
+const ListContainer = styled.div`
+  width: 36rem;
+  height: calc(100vh - 50px);
+  border-top: 1px solid var(--black-150);
+  border-right: 1px solid var(--black-150);
+  border-bottom: 1px solid var(--black-150);
+  border-radius: 0 15px 15px 0;
   background-color: var(--white);
   box-shadow: var(--bs-lg);
+  padding: 1.5rem 0 1rem 1.5rem;
+  transition: 0.2s;
+  @media (max-width: 768px) {
+    transition: 0.2s;
+    width: 30rem;
+  }
 `;
-
 const ContainerWrap = styled.div`
-  display: list-item;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+const EmptyContainer = styled.div`
+  flex: 1 1 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-right: 36px;
+  .partition {
+    width: 14px;
+    border-radius: 7px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px inset, rgba(0, 0, 0, 0.1) 0px 7px 13px -3px inset,
+      rgba(0, 0, 0, 0.1) 0px -3px 0px, hsl(0, 0%, 100%, 0.1) 0 1px 0 0;
+    transition: 0.2s;
+    @media (max-width: 768px) {
+      transition: 0.2s;
+      width: 4px;
+    }
+  }
+`;
+const PharmContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 95px);
+`;
+const PharmHeadContainer = styled.div`
+  margin-right: 42px;
+  margin-bottom: 10px;
+  transition: 0.2s;
+  @media (max-width: 768px) {
+    transition: 0.2s;
+    margin-right: 22px;
+  }
+`;
+const PharmItemContainer = styled.div`
+  overflow-y: scroll;
+  height: calc(100vh - 135px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.05) 2px 2px 4px inset, rgba(0, 0, 0, 0.05) 0px 7px 7px -3px inset,
+    rgba(0, 0, 0, 0.05) 0px -7px 7px -3px inset;
 `;
 
-//input 컨테이너
+//input
 const SearchContainer = styled.div`
-  width: 36.688rem;
   display: flex;
   justify-content: center;
-  padding: 2rem 1.5rem 1rem 1.5rem;
+  margin: 10px 0;
   .searchIcon {
     width: 1.5rem;
     height: 1.5rem;
     display: inline-block;
     position: absolute;
-    opacity: 0.5;
+    opacity: 0.4;
     margin: 0.8rem;
   }
 `;
-
-//input 창
 const SearchInput = styled.input`
-  width: 24rem;
+  width: 26rem;
   height: 3rem;
   border-radius: 10px;
-  border: 5px solid var(--blue-500);
+  border: 3px solid var(--blue-300);
   outline: none;
   font-size: 1.1rem;
   padding-left: 2.8rem;
+  transition: 0.2s;
+  :focus {
+    border-color: var(--blue-400);
+    box-shadow: var(--wrapped-shadow);
+    transition: 0.2s;
+  }
 `;
 
-//필터부분
-const PharmHeadContainer = styled.div`
+//Buttons
+const ButtonContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin-right: 3.1rem;
-  margin-top: 1.8rem;
-  margin-left: 6.25rem;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin: 25px 5px 3px 5px;
 `;
-//필터 버튼들
-const FilterButtons = styled.div`
-  vertical-align: baseline;
-  border: 1px solid var(--black-350);
-  border-radius: 5px;
-  border: none;
-`;
-//필터버튼
-const FilterButton = styled.button`
-  background-color: var(--white);
-  border: none;
-  color: var(--black-350);
-  font-size: 0.9rem;
-  width: 6.25rem;
-  &.filter_close {
-    border-top-left-radius: 8px 8px;
-    border-bottom-left-radius: 8px 8px;
-    border-right: 1px solid var(--black-350);
-  }
-  &.filter_popular {
-    border-right: 1px solid var(--black-350);
-  }
-  &.filter_star {
-    border-top-right-radius: 8px 8px;
-    border-bottom-right-radius: 8px 8px;
-  }
-  &:hover {
-    color: var(--blue-500);
-  }
-`;
-
-//약국카드
-const PharmCard = styled.div`
-  width: 35rem;
-  height: 25rem;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  border-bottom: 1px solid var(--black-100);
-  .pharm_img {
-    background-color: var(--black-200);
-    width: 23.75rem;
-    height: 15.625rem;
-    display: flex;
-    justify-content: center;
-    margin: auto;
-    border-radius: 10px;
-  }
-`;
-//하트 버튼!!
-const HeartPosition = styled.div`
-  position: relative;
-`;
-const HeartButton = styled.button`
-  all: unset;
-  cursor: pointer;
-
-  img {
-    position: absolute;
-    right: 6rem;
-    top: 3rem;
-  }
-`;
-
-//약국 타이틀
-const PharmTitleBox = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const PharmName = styled.div`
-  font-size: 1.56rem;
-  font-weight: bold;
-  margin-right: 4rem;
-`;
-
-//태그전체
-const TagContainer = styled.div`
-  margin-top: 0.5rem;
-  margin-left: 5rem;
-  margin-bottom: 0.5rem;
-`;
-
 //우리약국 가기
 const Button = styled.button`
   all: unset;
   cursor: pointer;
+  margin-right: 88px;
   .ourpharm_img {
     margin-right: 0.3rem;
   }
   .ourpharm {
     font-size: 1rem;
     color: var(--blue-500);
+    transition: 0.2s;
+    &:hover {
+      font-weight: 600;
+      color: var(--blue-600);
+      transition: 0.2s;
+    }
+  }
+`;
+const FilterButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  .partition {
+    width: 1.4px;
+    height: 0.9rem;
+    background-color: var(--black-300);
+    margin: 0 15px;
+  }
+`;
+const FilterButton = styled.button`
+  cursor: pointer;
+  background-color: var(--white);
+  border: none;
+  color: var(--black-350);
+  font-size: 0.9rem;
+  transition: 0.2s;
+  &:hover {
+    color: var(--blue-500);
+    transition: 0.2s;
   }
 `;
