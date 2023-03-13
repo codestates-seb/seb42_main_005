03afb6ce0cb6c4fda9af208546ef9ede48c590a6
JiHyeong23 <ekdl1120@gmail.com>
2023-03-14 01:30:22 +0900

    BE_fix: ListGetReportedReviewDto수정

--- back/src/main/java/com/project/mainproject/review/dto/ListGetReportedReviewDto.java
+++ back/src/main/java/com/project/mainproject/review/dto/ListGetReportedReviewDto.java
@@ -1,10 +1,12 @@
 package com.project.mainproject.review.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
+@Getter
 public class ListGetReportedReviewDto {
     List<GetReportedReviewDto> reportedReview;
 
