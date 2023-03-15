bf68d30309accb2dab4a26796d188edfc2de64fe
SigLee <lgh941107@gmail.com>
2023-03-15 11:23:22 +0900

    BE_fit/ CommonExceptionCode 상태 변경 상태값 변경

--- back/src/main/java/com/project/mainproject/exception/CommonExceptionCode.java
+++ back/src/main/java/com/project/mainproject/exception/CommonExceptionCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum CommonExceptionCode implements ExceptionCode{
     INTERNAL_SERVER(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류"),
     ACCEPT_TYPE_ERROR(HttpStatus.NOT_ACCEPTABLE,"Accept 타입 오류"),
-    NOT_HAVE_BODY(HttpStatus.NOT_ACCEPTABLE,"Body가 존재하지 않습니다."),
-    PARAMETER_ERROR(HttpStatus.NOT_ACCEPTABLE,"파라미터가 문제가 있습니다.")
+    NOT_HAVE_BODY(HttpStatus.NOT_ACCEPTABLE,"RequestBody가 존재하지 않습니다."),
+    PARAMETER_ERROR(HttpStatus.NOT_ACCEPTABLE,"Parameter가 존재하지 않습니다.")
     ;
 
     private final HttpStatus httpStatus;
