b96a22b941173c7e9dc26df777fec8ca5b302edd
JudiPARK0416 <p.eunjung.0425@gmail.com>
2023-03-16 19:27:16 +0900

    FE_feat/findPW 화면구현

--- front/src/Pages/FindPW.tsx
+++ front/src/Pages/FindPW.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import styled from "styled-components";
 import Button from "../Components/Ul/Button";
-import Input from "../Components/Ul/Input";
+import SignUpInput from "../Components/SignUpForm/SignUpInput";
+// import { Validate } from "../Components/SignUpForm/Validation"
+import { BsPersonCircle } from "react-icons/bs";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
 export default function FindPW() {
+  // 일단은 onchange 에 에러가 생긴 것 처럼 구현해 뒀습니다!!! validate 로직으로 바꿔주시면 감사하겠습니다!
+
+const [randomError, setRandomError] = useState(false)
+
   return (
     <Wrapper>
       <LogoContainer>
@@ -15,9 +23,14 @@ export default function FindPW() {
           <br />
           <p>입력시 해당 이메일로 임시 비밀번호가 전송됩니다.</p>
         </Instruction>
-        <InputContainer>
-          <Input />
+        <InputContainer className={randomError ? "error" : ""}>
+          <BsPersonCircle className="inputimage" />
+          <SignUpInput type={"email"} name={"email"} placeholder={"이메일을 입력하세요."} onChange={()=>setRandomError(true)} />
         </InputContainer>
+        <AlertMsg className={randomError ? "error" : ""}>
+          <AiOutlineExclamationCircle />
+          유효성 검사 로직이 들어갈 자리입니다~!~!
+        </AlertMsg>
         <ButtonContainer>
           <Button color="blue" size="lg" text="입력" />
         </ButtonContainer>
@@ -27,8 +40,6 @@ export default function FindPW() {
 }
 
 const Wrapper = styled.main`
-  border: 1px solid green;
-  /*  */
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -38,51 +49,86 @@ const Wrapper = styled.main`
   padding-bottom: 100px;
 `;
 const LogoContainer = styled.header`
-  border: 1px solid red;
-  /*  */
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 50px;
+  margin-bottom: 40px;
   img {
     padding-right: 1rem;
     width: 3.5rem;
     height: 3rem;
   }
+  h1 {
+    color: var(--blue-600);
+    font-size: 2.2rem;
+  }
 `;
 const Container = styled.section`
-  border: 1px solid gainsboro;
-  /*  */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 300px;
-  width: 420px;
+  padding: 30px 20px;
   border-radius: 18px;
   box-shadow: var(--bs-lg);
   border: 1px solid var(--black-100);
 `;
 const Instruction = styled.h2`
-  border: 1px solid khaki;
-  /*  */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   line-height: 15px;
-  font-size: 18px;
+  margin: 10px 0;
+  font-size: 17px;
+  color: var(--black-400);
 `;
 const InputContainer = styled.section`
-  border: 1px solid firebrick;
-  /*  */
   margin: 20px 0px;
   width: 350px;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid var(--black-150);
+  margin-bottom: 0.8rem;
+  border-radius: 10px;
+  box-shadow: var(--bs-sm);
+  padding: 0 10px;
+  .inputimage {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1.7rem;
+    height: 1.7rem;
+    margin: auto;
+    color: var(--black-200);
+  }
+  :focus-within {
+    border-color: var(--blue-300);
+    box-shadow: var(--wrapped-shadow);
+    transition: 0.2s;
+  }
+  &.error:focus-within {
+    box-shadow: var(--wrapped-shadow-red);
+    border: 1px solid hsl(359, 46%, 66%);
+    transition: 0.2s;
+  }
 `;
 const ButtonContainer = styled.footer`
-  border: 1px solid firebrick;
-  /*  */
   display: flex;
   justify-content: center;
   width: 350px;
+  margin-top: 20px;
+`;
+const AlertMsg = styled.div`
+  display: none;
+  &.error {
+    display: block;
+    line-height: 0;
+    display: flex;
+    align-items: center;
+    font-weight: normal;
+    gap: 5px;
+    font-size: 15px;
+    color: var(--red);
+  }
 `;
