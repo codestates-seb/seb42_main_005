4928df515b13787c5c3888738eb25e410b43b782
yesolll <9595kys@gmail.com>
2023-03-25 16:36:02 +0900

    BE_Fix: 약사 회원가입 반려
    - @Modifying 추가

--- back/src/main/java/com/project/mainproject/user/repository/UserRepository.java
+++ back/src/main/java/com/project/mainproject/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.project.mainproject.user.repository;
 
 import com.project.mainproject.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,8 +15,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.userIdx in :userIdxs")
     List<User> findByIds(@Param("userIdxs") List<Long> userIdxs);
 
+    @Modifying // 추가했습니다 03/25 예솔
     @Query("delete from User u WHERE u.userIdx in :userIdxs")
-    void deleteUserByIdList(@Param("userIdx")List<Long> userIdxs);
+    void deleteUserByIdList(@Param("userIdxs")List<Long> userIdxs);
     Optional<User> findByEmail(String email);
 
 }
