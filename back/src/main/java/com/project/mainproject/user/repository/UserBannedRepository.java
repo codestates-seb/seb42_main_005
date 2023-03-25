9b31f2d7e52a40653f635b35bfa250bb23f232a8
yesolll <9595kys@gmail.com>
2023-03-25 18:34:55 +0900

    BE_Fix: 계정 복구 쿼리 @Modifying 추가

--- back/src/main/java/com/project/mainproject/user/repository/UserBannedRepository.java
+++ back/src/main/java/com/project/mainproject/user/repository/UserBannedRepository.java
@@ -2,6 +2,7 @@ package com.project.mainproject.user.repository;
 
 import com.project.mainproject.user.entity.UserBanned;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 public interface UserBannedRepository extends JpaRepository<UserBanned, Long> {
 
+    @Modifying
     @Query("DELETE  From UserBanned ub where ub.user.userIdx in :userIdxs")
     void usersDelete(@Param("userIdxs")List<Long> usersIdx);
 }
