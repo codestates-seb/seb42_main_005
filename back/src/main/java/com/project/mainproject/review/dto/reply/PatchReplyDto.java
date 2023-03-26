468f066bc83db6045c8ea7386dcd62c61dc08dae
SigLee <lgh941107@gmail.com>
2023-03-27 02:19:21 +0900

    BE_fit/ReviewStub 위치 변경 및 데이터 수정

--- back/src/main/java/com/project/mainproject/review/dto/reply/PatchReplyDto.java
+++ back/src/main/java/com/project/mainproject/review/dto/reply/PatchReplyDto.java
@@ -1,12 +1,14 @@
 package com.project.mainproject.review.dto.reply;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.util.Assert;
 
 @Getter
 @ToString
+@Builder
 @AllArgsConstructor
 public class PatchReplyDto {
     private Long storeIdx;
---
+++ back/src/test/java/com/project/mainproject/stub/ReviewStub.java
@@ -0,0 +1,170 @@
+package com.project.mainproject.stub;
+
+import com.project.mainproject.review.dto.*;
+import com.project.mainproject.review.dto.reply.PatchReplyDto;
+import com.project.mainproject.review.dto.reply.PostReplyDto;
+import com.project.mainproject.review.dto.reply.SimpleReplyDto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewStub {
+    /*
+    * ReviewController
+    * */
+
+    //request
+    public static PostCreateReviewDto getPostCreateReviewStub() {
+        return PostCreateReviewDto.builder()
+                .content("리뷰 본문에 들어갈 내용입니다. 알약 가격이 다른곳보다 비싸요")
+                .rating(2)
+                .userIdx(1L)
+                .storeIdx(1L)
+                .build();
+    }
+
+    public static PostUpdateReviewDto getPostUpdateReviewStub() {
+        return PostUpdateReviewDto.builder()
+                .content("리뷰 본문을수정할 내용입니다.")
+                .rating(4)
+                .userIdx(1L)
+                .storeIdx(2L)
+                .build();
+    }
+
+    public static PostReviewReportDto getPostReviewReportStub() {
+        return PostReviewReportDto.builder().userIdx(1L).reviewIdx(1L).content("신고 사유 입니다.").build();
+    }
+
+
+    //response
+    public static ListGetStoreReviewDto getStoreReviewStub() {
+        List<ListGetStoreReviewDto.StoreReviewDto> result = new ArrayList<>();
+        for (Long i = 1L; i < 3L; i++) {
+            ListGetStoreReviewDto.StoreReviewDto build = ListGetStoreReviewDto.StoreReviewDto.builder()
+                    .reviewIdx(1L)
+                    .content("본문 내용이 들어갑니다.")
+                    .rating(4)
+                    .reviewImage("이미지 경로가 들어갑니다")
+                    .userIdx(1L)
+                    .userName("김복자")
+                    .profileImage("사용자 프로필이미지 사진이 들어갑니다.")
+                    .createdAt(LocalDateTime.now())
+                    .modifiedAt(LocalDateTime.now())
+                    .replies(getReplyStub())
+                    .build();
+            result.add(build);
+        }
+        return ListGetStoreReviewDto.builder().storeReviews(result).build();
+    }
+
+    //리뷰 작성, 수정에 응답으로 사용된다.
+    public static SimpleReviewDto getSimpleReviewStub() {
+        return SimpleReviewDto.builder()
+                .reviewIdx(1L)
+                .storeIdx(1L)
+                .userIdx(1L)
+                .build();
+    }
+
+    public static ListGetUserReviewDto getListGetUserReviewStub() {
+        List<ListGetUserReviewDto.GetUserReviewDto> result = new ArrayList<>();
+
+        for (Long i = 1L; i < 3L; i++) {
+            ListGetUserReviewDto.GetUserReviewDto build = ListGetUserReviewDto.GetUserReviewDto.builder()
+                    .reviewIdx(1L)
+                    .storeName("약국이름")
+                    .rating(4)
+                    .modifiedAt(LocalDateTime.now())
+                    .storeIdx(1L)
+                    .content("이곳은 리뷰 본문입니다.")
+                    .build();
+            result.add(build);
+        }
+
+        return ListGetUserReviewDto.builder().reviews(result).build();
+    }
+
+    /*
+    * AdminReviewController
+    * */
+
+    //request
+    //삭제 전용
+    public static ReviewIdxDto getReviewIdxStub() {
+        List<ReviewIdxDto.ReviewIdxs> result = new ArrayList<>();
+
+        for (long i = 1L; i < 3; i++) {
+            ReviewIdxDto.ReviewIdxs reviewIdxs = new ReviewIdxDto.ReviewIdxs(i);
+            result.add(reviewIdxs);
+        }
+        return new ReviewIdxDto(result);
+    }
+
+
+    //response
+    public static ListReportedReviewDto getListReportedReviewStub() {
+        List<ListReportedReviewDto.ReportedReviewDto> result = new ArrayList<>();
+
+        for (long i = 1L; i < 3; i++) {
+            ListReportedReviewDto.ReportedReviewDto build = ListReportedReviewDto.ReportedReviewDto.builder()
+                    .reviewIdx(i)
+                    .reportCnt(2)
+                    .email("aLong@gmail.com")
+                    .createdAt(LocalDateTime.now())
+                    .content("이곳은 신고 본문 내용입니다.")
+                    .build();
+            result.add(build);
+        }
+        return ListReportedReviewDto.builder().reportedReviews(result).build();
+    }
+
+    /*
+    * ReviewReplyController
+    * */
+
+    //request
+
+    public static PostReplyDto getPostReplyStub() {
+        return PostReplyDto.builder().userIdx(1L)
+                .storeIdx(2L)
+                .content("본문입니다.")
+                .reviewIdx(3L)
+                .build();
+    }
+
+    public static PatchReplyDto getPatchReplyStub() {
+        return PatchReplyDto.builder().replyIdx(1L).storeIdx(2L).userIdx(3L).reviewIdx(4L).content("이것은 수정할 본문 내용입니다.").build();
+    }
+
+
+    //response
+
+    public static SimpleReplyDto getSimpleReplyStub() {
+        return SimpleReplyDto.builder().replyIdx(1L).userIdx(2L).build();
+    }
+
+
+    //내부 동작 메서드
+    private static List<ListGetStoreReviewDto.ReplyDto> getReplyStub() {
+        List<ListGetStoreReviewDto.ReplyDto> result = new ArrayList<>();
+        for (Long i = 1L; i < 3L; i++) {
+            ListGetStoreReviewDto.ReplyDto build = ListGetStoreReviewDto.ReplyDto.builder()
+                    .replyIdx(1L)
+                    .content("리플 본문 입니다.")
+                    .userIdx(1L)
+                    .userName("김상구")
+                    .profileImage("사용자 프로필 이미지")
+                    .createdAt(LocalDateTime.now())
+                    .build();
+            result.add(build);
+        }
+
+        return result;
+    }
+
+
+}
+
+
