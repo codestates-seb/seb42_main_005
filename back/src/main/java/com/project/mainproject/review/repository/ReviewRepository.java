7f170fa18bb040c1638047bb300dab221a7e31ad
yesolll <9595kys@gmail.com>
2023-03-28 17:26:30 +0900

    BE_Feat: 스케줄링_리뷰 블라인드 처리
    - 기준 신고 수: 10

--- back/src/main/java/com/project/mainproject/review/repository/ReviewRepository.java
+++ back/src/main/java/com/project/mainproject/review/repository/ReviewRepository.java
@@ -5,7 +5,9 @@ import com.project.mainproject.review.enums.ReviewStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,4 +24,22 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query(value = "SELECT r FROM Review r WHERE r.reviewIdx in :reviewIdx AND r.reviewStatus = :reviewStatus")
     List<Review> findAllByIdAndReviewStatus(List<Long> reviewIdx, ReviewStatus reviewStatus);
+
+    @Query(value = " SELECT r.reviewIdx" +
+                    "  FROM Review r " +
+                    " WHERE r.reviewStatus = 'POSTED' " +
+                    "     AND r.reviewIdx IN(SELECT rr.review.reviewIdx " +
+                    "                          FROM ReviewReport rr " +
+                    "                         WHERE rr.reportStatus = 'REGISTERED' " +
+                    "                      GROUP BY rr.review.reviewIdx " +
+                    "                        HAVING count(rr) >= :reportCount) "
+    )
+    List<Long> findReviewsIdxByRegisteredReportCount(@Param("reportCount") long reportCount);
+
+    @Modifying
+    @Query(value = " UPDATE Review r " +
+            "           SET r.reviewStatus = 'BLINDED' " +
+            "         WHERE r.reviewIdx IN :reviewsIdx "
+    )
+    int updatedReviewStatusToBlinded(List<Long> reviewsIdx);
 }
---
+++ back/src/main/java/com/project/mainproject/scheduler/review/ReviewScheduler.java
@@ -0,0 +1,35 @@
+package com.project.mainproject.scheduler.review;
+
+import com.project.mainproject.review.repository.ReviewReportRepository;
+import com.project.mainproject.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ReviewScheduler {
+
+    private final ReviewRepository reviewRepository;
+    private final ReviewReportRepository reviewReportRepository;
+
+    @Scheduled(cron = "0 25 * * * *")
+    @Transactional
+    public void blindReview() {
+        long reportCount = 10;
+
+        List<Long> reviewsIdx = reviewRepository.findReviewsIdxByRegisteredReportCount(reportCount);
+
+        int blindedCount = reviewRepository.updatedReviewStatusToBlinded(reviewsIdx);
+        int reportSuccessCount = reviewReportRepository.updatedReportStatusToSuccess(reviewsIdx);
+
+        log.info(blindedCount + "개의 리뷰가 블라인드 처리 되었습니다.");
+        log.info(reportSuccessCount + "개의 신고가 성공 처리 되었습니다.");
+    }
+
+}
