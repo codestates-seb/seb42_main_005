e2a1a9f27332390e951e3719574a032be2830479
yesolll <9595kys@gmail.com>
2023-03-28 22:18:41 +0900

    BE_Feat: 스케줄링_리뷰 주석
    추후 삭제 예정

--- back/src/main/java/com/project/mainproject/scheduler/review/ReviewScheduler.java
+++ back/src/main/java/com/project/mainproject/scheduler/review/ReviewScheduler.java
@@ -4,11 +4,7 @@ import com.project.mainproject.review.repository.ReviewReportRepository;
 import com.project.mainproject.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Slf4j
 @Component
@@ -17,18 +13,19 @@ public class ReviewScheduler {
     private final ReviewRepository reviewRepository;
     private final ReviewReportRepository reviewReportRepository;
 
-    @Scheduled(cron = "0 0 * * * *")
-    @Transactional
-    public void blindReview() {
-        long reportCount = 10;
-
-        List<Long> reviewsIdx = reviewRepository.findReviewsIdxByRegisteredReportCount(reportCount);
-
-        int blindedCount = reviewRepository.updatedReviewStatusToBlinded(reviewsIdx);
-        int reportSuccessCount = reviewReportRepository.updatedReportStatusToSuccess(reviewsIdx);
-
-        log.info(blindedCount + "개의 리뷰가 블라인드 처리 되었습니다.");
-        log.info(reportSuccessCount + "개의 신고가 성공 처리 되었습니다.");
-    }
+    // 신고 등록 시 10개 체크!
+//    @Scheduled(cron = "0 0 * * * *")
+//    @Transactional
+//    public void blindReview() {
+//        long reportCount = 10;
+//
+//        List<Long> reviewsIdx = reviewRepository.findReviewsIdxByRegisteredReportCount(reportCount);
+//
+//        int blindedCount = reviewRepository.updatedReviewStatusToBlinded(reviewsIdx);
+//        int reportSuccessCount = reviewReportRepository.updatedReportStatusToSuccess(reviewsIdx);
+//
+//        log.info(blindedCount + "개의 리뷰가 블라인드 처리 되었습니다.");
+//        log.info(reportSuccessCount + "개의 신고가 성공 처리 되었습니다.");
+//    }
 
 }
