115fabdef98fbe3b45ba3cfeaad1b1e098efa947
yesolll <9595kys@gmail.com>
2023-03-29 03:35:19 +0900

    BE_Fix: 신고 10개 이상 누적 시 블라인드 처리

--- back/src/main/java/com/project/mainproject/review/service/ReviewReportService.java
+++ back/src/main/java/com/project/mainproject/review/service/ReviewReportService.java
@@ -2,7 +2,7 @@ package com.project.mainproject.review.service;
 
 import com.project.mainproject.review.entity.Review;
 import com.project.mainproject.review.entity.ReviewReport;
-import com.project.mainproject.review.repository.ReviewReportRepository;
+import com.project.mainproject.review.enums.ReviewStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +14,14 @@ import static com.project.mainproject.review.enums.ReportStatus.REGISTERED;
 public class ReviewReportService {
 
     private final ReviewService reviewService;
-    private final ReviewReportRepository reportRepository;
 
     @Transactional
     public void createReport(Long storeIdx, ReviewReport reviewReport) {
         reviewReport.setReportStatus(REGISTERED);
         Review review = reviewService.findVerifiedReview(storeIdx, reviewReport.getReview().getReviewIdx());
         review.addReport(reviewReport);
+
+        if (review.getReportCnt() >= 9) // Transaction 종료 전 -> 9부터 검사
+            review.setReviewStatus(ReviewStatus.BLINDED);
     }
 }
