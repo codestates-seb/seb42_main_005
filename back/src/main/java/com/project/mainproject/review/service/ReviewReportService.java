b94053a0e4e16e4109c71a13add7bb1703515059
yesolll <9595kys@gmail.com>
2023-03-31 03:16:16 +0900

    BE_Fix: 약사 신고시 신고 +3

--- back/src/main/java/com/project/mainproject/review/service/ReviewReportService.java
+++ back/src/main/java/com/project/mainproject/review/service/ReviewReportService.java
@@ -3,6 +3,8 @@ package com.project.mainproject.review.service;
 import com.project.mainproject.review.entity.Review;
 import com.project.mainproject.review.entity.ReviewReport;
 import com.project.mainproject.review.enums.ReviewStatus;
+import com.project.mainproject.user.entity.User;
+import com.project.mainproject.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +16,15 @@ import static com.project.mainproject.review.enums.ReportStatus.REGISTERED;
 public class ReviewReportService {
 
     private final ReviewService reviewService;
+    private final UserService userService;
 
     @Transactional
     public void createReport(Long storeIdx, ReviewReport reviewReport) {
-        reviewReport.setReportStatus(REGISTERED);
+        User user = userService.validUser(reviewReport.getUser().getUserIdx());
         Review review = reviewService.findVerifiedReview(storeIdx, reviewReport.getReview().getReviewIdx());
+
+        reviewReport.setReportStatus(REGISTERED);
+        reviewReport.setUser(user);
         review.addReport(reviewReport);
 
         if (review.getReportCnt() >= 9) // Transaction 종료 전 -> 9부터 검사
