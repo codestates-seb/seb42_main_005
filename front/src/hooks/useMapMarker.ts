7520782a1e67013cac0912d0db2446ba10c10dac
BangBang-e <youngjae7133@gmail.com>
2023-04-01 16:30:47 +0900

    현위치버튼 수정

--- front/src/hooks/useMapMarker.ts
+++ front/src/hooks/useMapMarker.ts
@@ -130,7 +130,6 @@ export async function useSearch(
   totalPharmList: never[],
   setTotalPharmList: React.Dispatch<React.SetStateAction<never[]>>,
   makeMap: any,
-  onModalUp: () => void,
 ) {
   if (makeMap) {
     try {
@@ -161,6 +160,59 @@ export async function useSearch(
         makeMap.panTo(searchResultPos);
         return MarkerPharmacy;
       });
+
+      clusterer = new kakao.maps.MarkerClusterer({
+        map: makeMap,
+        averageCenter: true,
+        minLevel: 6,
+        minClusterSize: 1,
+        calculator: [10, 30, 50],
+        styles: [
+          {
+            //* calculator 각 사이 값 마다 적용될 스타일을 지정한다
+            width: "70px",
+            height: "70px",
+            background: "hsla(241, 75%, 86%, 0.8)",
+            borderRadius: "50%",
+            color: "#000",
+            textAlign: "center",
+            fontWeight: "bold",
+            lineHeight: "71px",
+          },
+          {
+            width: "80px",
+            height: "80px",
+            background: "hsla(241, 80%, 80%, 0.75)",
+            borderRadius: "50%",
+            color: "#000",
+            textAlign: "center",
+            fontWeight: "bold",
+            lineHeight: "81px",
+          },
+          {
+            width: "90px",
+            height: "90px",
+            background: "hsla(241, 73%, 68%, 0.7)",
+            borderRadius: "50%",
+            color: "#000",
+            textAlign: "center",
+            fontWeight: "bold",
+            lineHeight: "91px",
+          },
+          {
+            width: "100px",
+            height: "100px",
+            background: "hsla(241, 88%, 67%, 0.65)",
+            borderRadius: "50%",
+            color: "#000",
+            textAlign: "center",
+            fontWeight: "bold",
+            lineHeight: "101px",
+          },
+        ],
+      });
+      clusterer.addMarkers(markers);
+      clusterer.setMap(makeMap);
     } catch (error) {
       console.log(error);
     }
